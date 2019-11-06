52185af5b65840b802a01dd2e2316c46da2ac983
Mohamed Sallam <sallamx99@gmail.com>
2019-11-06 16:09:00 +0200

    set content instead of reading a url

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const puppeteer = require("puppeteer");
-const { writeFileSync } = require("fs");
+const { writeFileSync, readFileSync } = require("fs");
 
 async function printPDF() {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto(`file://${__dirname}/test.html`, {
-    waitUntil: "networkidle0"
-  });
+  await page.setContent(readFileSync(`${__dirname}/test.html`, 'utf-8'));
   const pdfPromise = page.pdf({ format: "A4" });
   const pdf = await pdfPromise
   await browser.close();
@@ -14,5 +12,5 @@ async function printPDF() {
 }
 
 (async function() {
-  writeFileSync(`${__dirname}/test.pdf`, await printPDF());
+  writeFileSync(`${__dirname}/test2.pdf`, await printPDF());
 })();
